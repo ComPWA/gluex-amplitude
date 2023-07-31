dd2a9967b26daf3865791ceb0138a41287bb1e3c
Amy Schertz <amschertz@email.wm.edu>
2023-07-31 16:53:45 -0400

    update print_amplitudes to use Zlm amplitude class, remove config file

--- script/print_amplitudes.cc
+++ script/print_amplitudes.cc
@@ -9,13 +9,7 @@
 
 #include "TSystem.h"
 
-#include "AMPTOOLS_AMPS/BreitWigner.h"
-#include "AMPTOOLS_AMPS/Vec_ps_refl.h"
-#include "AMPTOOLS_AMPS/PhaseOffset.h"
-#include "AMPTOOLS_AMPS/ComplexCoeff.h"
-#include "AMPTOOLS_AMPS/OmegaDalitz.h"
-#include "AMPTOOLS_AMPS/Piecewise.h"
-#include "AMPTOOLS_AMPS/LowerVertexDelta.h"
+#include "AMPTOOLS_AMPS/Zlm.h"
 
 #include "MinuitInterface/MinuitMinimizationManager.h"
 #include "IUAmpTools/AmpToolsInterface.h"
@@ -42,10 +36,7 @@ int main( int argc, char* argv[] ){
 	ConfigurationInfo* cfgInfo = parser.getConfigurationInfo();
 	cfgInfo->display();
 
-	AmpToolsInterface::registerAmplitude( BreitWigner() );
-	AmpToolsInterface::registerAmplitude( OmegaDalitz() );
-	AmpToolsInterface::registerAmplitude( Vec_ps_refl() );
-	AmpToolsInterface::registerAmplitude( LowerVertexDelta() );
+	AmpToolsInterface::registerAmplitude( Zlm() );
 
 	AmpToolsInterface ati( cfgInfo );
 	vector< TLorentzVector > p4List;
@@ -53,11 +44,8 @@ int main( int argc, char* argv[] ){
 	// list lab frame p4s, in the order specified in the AmpTools config file
 	p4List.push_back( TLorentzVector(0, 0, 8.209470, 8.209470) ); //beam photon
 	p4List.push_back( TLorentzVector(-0.291810, -0.315238, 0.695296, 1.244320) ); //recoil proton
-	p4List.push_back( TLorentzVector(0.183147, 0.002392, 0.384435, 0.448137) ); //bachelor pion
-	p4List.push_back( TLorentzVector(0.151104, 0.333728, 1.914300, 1.953710) ); //pi0 from omega
-	p4List.push_back( TLorentzVector(-0.135341, 0.038805, 2.134320, 2.143510) ); //pi+ from omega
-	p4List.push_back( TLorentzVector(0.076385, 0.004007, 3.173990, 3.177980) ); //pi- from omega
-	p4List.push_back( TLorentzVector(0.016522, -0.063693, -0.092871, 0.180096) ); //recoil pion
+	p4List.push_back( TLorentzVector(0.183147, 0.002392, 0.384435, 0.448137) ); //resonance daughter 1
+	p4List.push_back( TLorentzVector(0.151104, 0.333728, 1.914300, 1.953710) ); //resonance daughter 2
 
 	Kinematics kin( p4List );
 
