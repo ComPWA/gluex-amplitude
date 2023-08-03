b84615820db2f8427264f5acae35039af191c997
Lawrence Ng <lng1492@gmail.com>
2023-08-03 16:23:16 -0400

    FIX: merging problems, using uproot

--- script/makeComparison.py
+++ script/makeComparison.py
@@ -1,11 +1,7 @@
 import re
 
 import numpy as np
-<<<<<<< HEAD
 import uproot
-=======
-import ROOT
->>>>>>> 35a193aef6d55268338deed29107e660f57b89f8
 
 # Load the data from the root file
 data = uproot.open("data.root")["kin"].arrays(
@@ -25,7 +21,6 @@ assert len(intensities) == len(data["cosTheta_eta_hel_thrown"])
 
 # Rename dictionary keys
 data["intensity"] = intensities
-<<<<<<< HEAD
 data = data.rename(
     {
         "cosTheta_eta_hel_thrown": "theta",
@@ -33,16 +28,6 @@ data = data.rename(
         "Phi_thrown": "Phi",
     },
     axis=1,
-=======
-data["theta"] = np.arccos(data.pop("cosTheta_eta_hel_thrown"))
-data["phi"] = data.pop("phi_eta_hel_thrown")
-data["Phi"] = data.pop("Phi_thrown") / (2 * np.pi)
-
-# Convert the dictionary to a structured array
-structured_array = np.empty(
-    len(next(iter(data.values()))),
-    dtype=[(key, data[key].dtype) for key in data.keys()],
->>>>>>> 35a193aef6d55268338deed29107e660f57b89f8
 )
 
 # Standardize the data to be angles in radians
