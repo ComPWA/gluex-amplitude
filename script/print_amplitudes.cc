62ebf2e83f48ee1825737a9c025561257bfe52ac
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-08-04 10:04:09 -0400

    DX: enforce `clang-format` formatting (#14)
    * DX: activate format on save in C++ files
    * DX: activate Git autofetch
    * DX: add `.clang-format` config
    * DX: add `clang-format` as pre-commit hook
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- script/print_amplitudes.cc
+++ script/print_amplitudes.cc
@@ -1,55 +1,60 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include <complex>
-#include <string>
-#include <vector>
-#include <utility>
+#include <fstream>
+#include <iostream>
 #include <map>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "TSystem.h"
 
 #include "AMPTOOLS_AMPS/Zlm.h"
 
-#include "MinuitInterface/MinuitMinimizationManager.h"
 #include "IUAmpTools/AmpToolsInterface.h"
-#include "IUAmpTools/FitResults.h"
 #include "IUAmpTools/ConfigFileParser.h"
 #include "IUAmpTools/ConfigurationInfo.h"
+#include "IUAmpTools/FitResults.h"
+#include "MinuitInterface/MinuitMinimizationManager.h"
 
-int main( int argc, char* argv[] ){
+int main(int argc, char *argv[]) {
 
-	string configfile;
+  string configfile;
 
-	for ( int i = 1; i < argc; i++ ){
-		string arg( argv[i] );
+  for (int i = 1; i < argc; i++) {
+    string arg(argv[i]);
 
-		if ( arg == "-c" ){ configfile = argv[++i]; }
-	}
+    if (arg == "-c") {
+      configfile = argv[++i];
+    }
+  }
 
-	if( configfile.size() == 0 ){
-		cout << "No config file specified" << endl;
-		exit(1);
-	}
+  if (configfile.size() == 0) {
+    cout << "No config file specified" << endl;
+    exit(1);
+  }
 
-	ConfigFileParser parser(configfile);
-	ConfigurationInfo* cfgInfo = parser.getConfigurationInfo();
-	cfgInfo->display();
+  ConfigFileParser parser(configfile);
+  ConfigurationInfo *cfgInfo = parser.getConfigurationInfo();
+  cfgInfo->display();
 
-	AmpToolsInterface::registerAmplitude( Zlm() );
+  AmpToolsInterface::registerAmplitude(Zlm());
 
-	AmpToolsInterface ati( cfgInfo );
-	vector< TLorentzVector > p4List;
+  AmpToolsInterface ati(cfgInfo);
+  vector<TLorentzVector> p4List;
 
-	// list lab frame p4s, in the order specified in the AmpTools config file
-	p4List.push_back( TLorentzVector(0, 0, 8.209470, 8.209470) ); //beam photon
-	p4List.push_back( TLorentzVector(-0.291810, -0.315238, 0.695296, 1.244320) ); //recoil proton
-	p4List.push_back( TLorentzVector(0.183147, 0.002392, 0.384435, 0.448137) ); //resonance daughter 1
-	p4List.push_back( TLorentzVector(0.151104, 0.333728, 1.914300, 1.953710) ); //resonance daughter 2
+  // list lab frame p4s, in the order specified in the AmpTools config file
+  p4List.push_back(TLorentzVector(0, 0, 8.209470, 8.209470)); // beam photon
+  p4List.push_back(TLorentzVector(-0.291810, -0.315238, 0.695296,
+                                  1.244320)); // recoil proton
+  p4List.push_back(TLorentzVector(0.183147, 0.002392, 0.384435,
+                                  0.448137)); // resonance daughter 1
+  p4List.push_back(TLorentzVector(0.151104, 0.333728, 1.914300,
+                                  1.953710)); // resonance daughter 2
 
-	Kinematics kin( p4List );
+  Kinematics kin(p4List);
 
-	ati.printEventDetails( cfgInfo->reactionList()[0]->reactionName(), &kin );
+  ati.printEventDetails(cfgInfo->reactionList()[0]->reactionName(), &kin);
 
-	return 0;
+  return 0;
 }
